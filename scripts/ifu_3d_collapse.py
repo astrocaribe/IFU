45036f4fff6ef7ae322033dbff43c9630bc497a6
astrocaribe <tlcommodore@gmail.com>
2014-07-17 15:50:35 -0400

    Edits to the sigma clipping functionality of this script. Errors w/ actual sigma clipping to be tested soon.

--- scripts/ifu_3d_collapse.py
+++ scripts/ifu_3d_collapse.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import numpy as np
-from astropy.io import fits
 from astropy.stats.funcs import sigma_clip
 
 import sys
@@ -52,17 +51,21 @@ def ifu_3d_collapse(array_in, sect=[0,0], method='sum', sigma=False):
     
     if sigma:
         # Subtract the collapsed array from the input datacube
-        subtracted_array = ifu_math(slice_array, collapsed_array, method='subtract')
-        print()
-        print('(3d_collapse): Subtracted array shape:', subtracted_array.shape)
+    #    subtracted_array = ifu_math(slice_array, collapsed_array, method='subtract')
+    #    print()
+    #    print('(3d_collapse): Subtracted array shape:', subtracted_array.shape)
         
         # Perform sigma-clip on subtracted cube
         print('(3d_collapse): Clipping data...')
-        clipped_array = sigma_clip(subtracted_array, sigma, iters=None, axis=0, copy=False)
+        #clipped_array = sigma_clip(subtracted_array, sigma, iters=None, axis=0, copy=False)
+        clipped_array = sigma_clip(slice_array, sigma, iters=None, axis=0, copy=False)
         print()
         print('(3d_collapse): Array shape:', clipped_array.shape)
         
-        if np.array_equal(clipped_array, subtracted_array): print('Damn, arrays are exactly the same!')
+    #    if np.array_equal(clipped_array, subtracted_array): print('Damn, arrays are exactly the same!')
+        if not np.array_equal(clipped_array, slice_array): 
+            raise TypeError
+            print('Damn, arrays are exactly the same!')    
         
         # Now that we have the sigma mask, recalculate the collapse...
         collapsed_array = arrayCollapse(clipped_array, method)    
