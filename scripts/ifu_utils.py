a6cb1356f8703e28cb2c9bf8aedbf622a0557b4c
astrocaribe <tlcommodore@gmail.com>
2014-08-21 18:09:16 -0400

    Edits to docstrings for Sphinx/Numpydoc documentation.

--- scripts/ifu_utils.py
+++ scripts/ifu_utils.py
@@ -2,10 +2,27 @@
 import numpy as np
 
 def frame_convert(frame, cals):
-    ''' 
+    """ 
     Convert the frame to a wavelength using a starting value and 
-    delta (in microns) specific to the NGC4151_Hband.fits file
-    '''
+    delta (in microns).
+    
+    Parameters
+    ----------
+    frame : int
+        Datacube frame to convert to a wavelength.
+    
+    cals : tuple of ints
+        Calibration constants for the given datacube.
+        
+    Returns
+    -------
+    wave : float
+        The converted wavelength of the datacube frame.
+        
+    Raises
+    ------
+    Not yet implemented.
+    """
     
     # Calculate the frame conversion
     wave = (frame - cals[0]) * cals[2] + cals[1]
@@ -13,10 +30,27 @@ def frame_convert(frame, cals):
     return wave # [format(z, '.3f') for z in wave] #
 
 def wave_convert(wave, cals):
-    '''
+    """
     Convert the wavelength to a frame using a starting value and 
-    delta (in microns) specific to the NGC4151_Hband.fits file 
-    '''
+    delta (in microns).
+    
+    Parameters
+    ----------
+    wave : float
+        The wavelength to convert to a datacube frame.
+    
+    cals : tuple of ints
+        Calibration constants for the given datacube.
+        
+    Returns
+    -------
+    frame : int
+        Datacube frame to converted from the input wavelength.
+        
+    Raises
+    ------
+    Not yet implemented.
+    """
     
     from numpy import round
     from numpy import int
