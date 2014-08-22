442701b941bfd8122b9eb35520bdc7473ea5c216
astrocaribe <tlcommodore@gmail.com>
2014-08-22 18:30:42 -0400

    switched upper/lower x axes in spectrum plot. doctring edits.

--- scripts/ifu_1d_spectrum.py
+++ scripts/ifu_1d_spectrum.py
@@ -10,9 +10,38 @@ from ifu_math import *
 from ifu_utils import *
 
 def ifu_1d_spectrum(array_in, spaxel, cals, trim=100, continuum=False, display=False):
-    '''
-    Description:
-    '''
+    """
+    Extract the spectrum of a gixen spaxel from a given datacube.
+    
+    Parameters
+    ----------
+    array_in : numpy.ndarray
+               Input datacube.
+               
+    spaxel : tuple of ints
+             The spaxel (i.e., the [x, y] pixel location) for 
+             which to perform the extraction.
+             
+    cals : tuple of floats
+           Calibration values for the input datacube; 
+           (crpix, crval, crdelt).
+           
+    trim : int
+           Integer amount to trim from begin/end of the datacube.
+           
+    continuum : bool, optional
+                Perforn a continuum subtraction (default is False, 
+                no continuum subtraction is done).
+    
+    display : bool, optional
+              Display the extracted spectrum (default is False,
+              spectrum is not displayed).
+    
+    Returns
+    -------
+    spectrum : numpy.ndarray
+               The extracted spectrum.
+    """
     
     # Create an array of frame indeces from length of input array
     frame_range = np.arange(len(array_in))
@@ -40,15 +69,13 @@ def ifu_1d_spectrum(array_in, spaxel, cals, trim=100, continuum=False, display=F
         
         fig, ax = plt.subplots(figsize=(10, 4))
         
-        ax.set_xlim(0, array_in.shape[0])
-        ax.plot(spec_frame, spectrum)
-        ax.set_xlabel('Datacube Frame ($z$)')
-        ax.set_ylabel('Counts')
+        ax.set_xlim(spec_wave[0], spec_wave[-1])
+        ax.plot(spec_wave, spectrum)
+        ax.set_xlabel('Wavelength ($\mu m$)')
         
         ax_twin = ax.twiny()
-        ticks = frame_range[::500]
+        ticks = frame_range[::250]
         ax_twin.set_xticks(ticks, minor=True)
-        ax_twin.set_xticklabels(np.round(frame_convert(ticks, cals), 3))
-        ax_twin.set_xlabel('Wavelength ($\mu m$)')
+        ax_twin.set_xlabel('Datacube Frame')
             
     return spectrum
