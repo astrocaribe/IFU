ae6a5c47ade8ee00db0cf8e2d2cfeaf61e8565cf
astrocaribe <leblanc@stsci.edu>
2014-08-29 06:33:10 -0400

    Added a spectrum input variable. More work to be done!

--- scripts/ifu_lineMeasure.py
+++ scripts/ifu_lineMeasure.py
@@ -4,10 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-import sys
-sys.path.append('./scripts/')
-
-def ifu_1d_spectrum(spectrum, region=[], display=False):
+def ifu_lineMeasure(spectrum, region=[], display=False):
     """
     Measure the line (if present) in a given region of an 
     input spectrum.
@@ -33,5 +30,12 @@ def ifu_1d_spectrum(spectrum, region=[], display=False):
            Line location (wavelength), FWHM, and intensity.
     
     """
+    inSpectrum = 0
     
-    return line
+    if len(region) == 2:
+        inSpectrum = spectrum[region[0]:region[1]]
+    else:
+        print('The spectral region must be entered as [a, b], \
+        where a and b are the lower/upper limits!')
+    
+    return inSpectrum
