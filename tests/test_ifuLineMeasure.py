b253cc10a4d2625b082001602db0044974b9d7a0
astrocaribe <tlcommodore@gmail.com>
2014-09-03 19:34:13 -0400

    Tweaks to test suite.

--- tests/test_ifuLineMeasure.py
+++ tests/test_ifuLineMeasure.py
@@ -31,7 +31,7 @@ def test_inputSpectra():
     1. Test the input spectrum format.
     """
     
-    testSpec = ifu_1d_spectrum(cube, [[30,30]], cals, trim=100, continuum=False, display=False)
+    testSpec = extractSpectrum(cube, [[30,30]], cals, trim=100, continuum=False, display=False)
     line = lineMeasure(testSpec, region=[800, 1200])
     assert len(line) > 0 and line[0] != 0
     
@@ -41,6 +41,6 @@ def test_inputRange():
     2. Test the range input past limits.
     """
     
-    testSpec = ifu_1d_spectrum(cube, [[30,30]], cals, trim=100, continuum=False, display=False)
+    testSpec = extractSpectrum(cube, [[30,30]], cals, trim=100, continuum=False, display=False)
     line = lineMeasure(testSpec, region=[3000, 3100])
-    assert len(line) > 0 and line[0] != 0
+    assert line == 0
